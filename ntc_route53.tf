8a5fa1ac3f90d4a3581a15f5fcc40ac6c98a0bd4
stefano-franco <stefano.franco@nuvibit.com>
2023-10-18 17:47:49 +0200

    update examples

--- ntc_route53.tf
+++ ntc_route53.tf
@@ -2,54 +2,55 @@
 # ¦ LOCALS
 # ---------------------------------------------------------------------------------------------------------------------
 locals {
-  # name of the route53 hosted zone
-  route53_zone_name          = "mydomain.internal"
-  route53_zone_description   = "Managed by Terraform"
-  # 
-  route53_zone_force_destroy = false
-  # private hosted zones require at least one vpc to be associated
-  # public hosted zones cannot have any vpc associated
-  route53_zone_type          = "private"
-  route53_zone_associated_vpc_ids = [
-    module.ntc_vpc_prod_stage.vpc_id
-  ]
+  route53_mydomain_internal = {
+    # name of the route53 hosted zone
+    zone_name        = "mydomain.internal"
+    zone_description = "Managed by Terraform"
+    # 
+    zone_force_destroy = false
+    # private hosted zones require at least one vpc to be associated
+    # public hosted zones cannot have any vpc associated
+    zone_type = "private"
+    zone_associated_vpc_ids = [
+      module.ntc_vpc_prod_stage.vpc_id
+    ]
 
-  # list of dns records which should be created in hosted zone. alias records are a special type of records
-  # https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/resource-record-sets-choosing-alias-non-alias.html
-  route53_dns_records = [
-    {
-      name = ""
-      type = "A"
-      ttl  = 300
-      values = [
-        "192.168.1.1",
-        "192.168.2.2"
-      ]
-    },
-    {
-      name = "nuvibit"
-      type = "CNAME"
-      ttl  = 300
-      values = [
-        "nuvibit.com"
-      ]
-    },
-    {
-      name = "logs-endpoint"
-      type = "CNAME"
-      ttl  = 300
-      alias = {
-        enable_alias = true
-        target_domain_name     = module.ntc_vpc_prod_stage.interface_endpoints[0].
-        target_hosted_zone_id  = module.ntc_vpc_prod_stage.interface_endpoints[0].
-        evaluate_target_health = true
-        
+    # list of dns records which should be created in hosted zone. alias records are a special type of records
+    # https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/resource-record-sets-choosing-alias-non-alias.html
+    dns_records = [
+      {
+        name = ""
+        type = "A"
+        ttl  = 300
+        values = [
+          "192.168.1.1",
+          "192.168.2.2"
+        ]
+      },
+      {
+        name = "nuvibit"
+        type = "CNAME"
+        ttl  = 300
+        values = [
+          "nuvibit.com"
+        ]
+      },
+      {
+        name = "cloudwatch-endpoint"
+        type = "A"
+        ttl  = 300
+        alias = {
+          enable_alias           = true
+          target_dns_name        = module.ntc_vpc_prod_stage.interface_endpoints.logs.dns_names[0]
+          target_hosted_zone_id  = module.ntc_vpc_prod_stage.interface_endpoints.logs.hosted_zone_id
+          evaluate_target_health = true
+        }
       }
-    }
-  ]
+    ]
 
-  # (optional) 
-  route53_zone_delegation_list = []
+    # (optional) 
+    zone_delegation_list = []
+  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -58,13 +59,13 @@ locals {
 module "ntc_route53_mydomain_internal" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-route53?ref=beta"
 
-  zone_name               = local.route53_zone_name
-  zone_type               = local.route53_zone_type
-  zone_associated_vpc_ids = local.route53_zone_associated_vpc_ids
-  zone_force_destroy      = local.route53_zone_force_destroy
-  zone_description        = local.route53_zone_description
-  zone_delegation_list    = local.route53_zone_delegation_list
-  dns_records             = local.route53_dns_records
+  zone_name               = local.route53_mydomain_internal.zone_name
+  zone_description        = local.route53_mydomain_internal.zone_description
+  zone_type               = local.route53_mydomain_internal.zone_type
+  zone_associated_vpc_ids = local.route53_mydomain_internal.zone_associated_vpc_ids
+  zone_force_destroy      = local.route53_mydomain_internal.zone_force_destroy
+  zone_delegation_list    = local.route53_mydomain_internal.zone_delegation_list
+  dns_records             = local.route53_mydomain_internal.dns_records
 
   providers = {
     aws = aws.euc1
---
+++ ntc_route53_central_endpoints.tf
@@ -0,0 +1,77 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ LOCALS
+# ---------------------------------------------------------------------------------------------------------------------
+locals {
+  # for centralized vpc interface endpoints a dedicated private hosted zone is required for each endpoint (ec2 -> ec2.eu-central-2.amazonaws.com)
+  # these private hosted zones need to be associated to all vpcs which should access the centralized endpoints
+  # https://docs.aws.amazon.com/whitepapers/latest/building-scalable-secure-multi-vpc-network-infrastructure/centralized-access-to-vpc-private-endpoints.html
+
+  route53_central_endpoints = {
+    for endpoint in module.ntc_vpc_prod_stage.interface_endpoints : endpoint.common_name => {
+      # name of the route53 hosted zone
+      zone_name        = endpoint.private_dns_name
+      zone_description = "Managed by Terraform"
+      # 
+      zone_force_destroy = true
+      # private hosted zones require at least one vpc to be associated
+      # public hosted zones cannot have any vpc associated
+      zone_type = "private"
+      zone_associated_vpc_ids = [
+        module.ntc_vpc_prod_stage.vpc_id
+      ]
+
+      # list of dns records which should be created in hosted zone. alias records are a special type of records
+      # https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/resource-record-sets-choosing-alias-non-alias.html
+      dns_records = [
+        {
+          name = ""
+          type = "A"
+          ttl  = 300
+          alias = {
+            enable_alias           = true
+            target_dns_name        = endpoint.dns_names[0]
+            target_hosted_zone_id  = endpoint.hosted_zone_id
+            evaluate_target_health = true
+          }
+        },
+        {
+          name = "*"
+          type = "A"
+          ttl  = 300
+          alias = {
+            enable_alias           = true
+            target_dns_name        = endpoint.dns_names[0]
+            target_hosted_zone_id  = endpoint.hosted_zone_id
+            evaluate_target_health = true
+          }
+        }
+      ]
+
+      # (optional) 
+      zone_delegation_list = []
+    }
+    # private dns must be disabled for centralized vpc endpoints
+    if endpoint.private_dns_enabled == false
+  }
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ NTC ROUTE53
+# ---------------------------------------------------------------------------------------------------------------------
+module "ntc_route53_central_endpoints" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-route53?ref=beta"
+
+  for_each = local.route53_central_endpoints
+
+  zone_name               = each.value.zone_name
+  zone_description        = each.value.zone_description
+  zone_type               = each.value.zone_type
+  zone_associated_vpc_ids = each.value.zone_associated_vpc_ids
+  zone_force_destroy      = each.value.zone_force_destroy
+  zone_delegation_list    = each.value.zone_delegation_list
+  dns_records             = each.value.dns_records
+
+  providers = {
+    aws = aws.euc1
+  }
+}
