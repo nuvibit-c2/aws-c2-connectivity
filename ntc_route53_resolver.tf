98a006ce0d6ba11695aa1c73ccac4350ed205de5
stefano-franco <stefano.franco@nuvibit.com>
2023-10-23 22:47:34 +0200

    update example

--- ntc_route53_resolver.tf
+++ ntc_route53_resolver.tf
@@ -6,11 +6,10 @@ locals {
   # https://docs.aws.amazon.com/whitepapers/latest/hybrid-cloud-dns-options-for-vpc/route-53-resolver-endpoints-and-forwarding-rules.html
 
   route53_resolver = {
-    # ROUTE53 INBOUND RESOLVER
-    # configure conditional dns forwarding from on-premise dns servers to these endpoints (e.g. *.cloud.mydomain.internal)
-    resolver_endpoints_inbound_config = {
-      create_endpoints = true
-      resolver_name    = "r53-inbound-resolver"
+    # inbound resolver endpoints are required for conditional dns forwarding from on-premise dns servers to aws
+    resolver_endpoint_inbound = {
+      create_endpoint = true
+      resolver_name   = "route53-inbound-resolver-endpoint"
       # (optional) you can attach existing security groups instead of creating a new one
       security_group_ids = []
       subnets = [for index, id in module.ntc_vpc_central_endpoints.subnet_ids["hybrid-private"] :
@@ -22,12 +21,28 @@ locals {
       ]
     }
 
-    # for inbound resolver only inbound traffic is allowed (by default tcp/udp 53)
-    resolver_endpoints_inbound_security_group = {
+    # outbound resolver endpoints are required for dns forwarding rules from aws to on-premise dns servers
+    resolver_endpoint_outbound = {
+      create_endpoint = true
+      resolver_name   = "route53-outbound-resolver-endpoint"
+      # (optional) you can attach existing security groups instead of creating a new one
+      security_group_ids = []
+      subnets = [for index, id in module.ntc_vpc_central_endpoints.subnet_ids["hybrid-private"] :
+        {
+          subnet_id = id
+          # (optional but recommended) set a static ip for the resolver endpoint
+          static_ip = null
+        }
+      ]
+    }
+
+    # only allow dns traffic for resolver endpoints (by default tcp/udp 53)
+    resolver_endpoint_security_group = {
       create_security_group = true
-      name                  = "r53-inbound-resolver"
-      description           = "DNS traffic to inbound resolvers"
+      name                  = "route53-resolver-endpoint-sg"
+      description           = "DNS traffic inbound and outbound resolvers"
       vpc_id                = module.ntc_vpc_central_endpoints.vpc_id
+
       ingress_rules = [
         {
           protocol = "tcp"
@@ -37,7 +52,7 @@ locals {
             "192.168.9.9/32"
           ]
           # (optional) allow outbound resolver to forward to inbound resolver
-          outbound_to_inbound_forwarding = true
+          self = true
         },
         {
           protocol = "udp"
@@ -47,32 +62,10 @@ locals {
             "192.168.9.9/32"
           ]
           # (optional) allow outbound resolver to forward to inbound resolver
-          outbound_to_inbound_forwarding = true
+          self = true
         }
       ]
-    }
 
-    # ROUTE53 OUTBOUND RESOLVER
-    resolver_endpoints_outbound_config = {
-      create_endpoints = true
-      resolver_name    = "r53-outbound-resolver"
-      # (optional) you can attach existing security groups instead of creating a new one
-      security_group_ids = []
-      subnets = [for index, id in module.ntc_vpc_central_endpoints.subnet_ids["hybrid-private"] :
-        {
-          subnet_id = id
-          # (optional but recommended) set a static ip for the resolver endpoint
-          static_ip = null
-        }
-      ]
-    }
-
-    # for outbound resolver only outbound traffic is allowed (by default tcp/udp 53)
-    resolver_endpoints_outbound_security_group = {
-      create_security_group = true
-      name                  = "r53-outbound-resolver"
-      description           = "DNS traffic from outbound resolvers"
-      vpc_id                = module.ntc_vpc_central_endpoints.vpc_id
       egress_rules = [
         {
           protocol = "tcp"
@@ -82,7 +75,7 @@ locals {
             "192.168.9.9/32"
           ]
           # (optional) allow outbound resolver to forward to inbound resolver
-          outbound_to_inbound_forwarding = true
+          self = true
         },
         {
           protocol = "udp"
@@ -92,7 +85,7 @@ locals {
             "192.168.9.9/32"
           ]
           # (optional) allow outbound resolver to forward to inbound resolver
-          outbound_to_inbound_forwarding = true
+          self = true
         }
       ]
     }
@@ -105,10 +98,9 @@ locals {
 module "ntc_route53_resolver" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-route53//modules/resolver?ref=beta"
 
-  resolver_endpoints_inbound_config          = local.route53_resolver.resolver_endpoints_inbound_config
-  resolver_endpoints_outbound_config         = local.route53_resolver.resolver_endpoints_outbound_config
-  resolver_endpoints_inbound_security_group  = local.route53_resolver.resolver_endpoints_inbound_security_group
-  resolver_endpoints_outbound_security_group = local.route53_resolver.resolver_endpoints_outbound_security_group
+  resolver_endpoint_inbound        = local.route53_resolver.resolver_endpoint_inbound
+  resolver_endpoint_outbound       = local.route53_resolver.resolver_endpoint_outbound
+  resolver_endpoint_security_group = local.route53_resolver.resolver_endpoint_security_group
 
   providers = {
     aws = aws.euc1
