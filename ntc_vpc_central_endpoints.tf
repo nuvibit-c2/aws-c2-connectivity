e2c3892134868c1740e182a12f893e5f87a0f5e8
stefano-franco <stefano.franco@nuvibit.com>
2024-01-23 15:17:00 +0100

    update references

--- ntc_vpc_central_endpoints.tf
+++ ntc_vpc_central_endpoints.tf
@@ -116,11 +116,11 @@ module "ntc_vpc_central_endpoints" {
         transit_gateway_default_route_table_propagation = false
         transit_gateway_id                              = aws_ec2_transit_gateway.ntc_core.id
         # vpc attachement can only be associated with a single transit gateway route table
-        transit_gateway_association_with_route_table_id = aws_ec2_transit_gateway_route_table.hub.id
+        transit_gateway_association_with_route_table_id = aws_ec2_transit_gateway_route_table.ntc_hub.id
         # vpc attachement can propagate to multiple transit gateway route table for dynamic routing
         transit_gateway_propagation_to_route_table_ids = [
-          aws_ec2_transit_gateway_route_table.hub.id,
-          aws_ec2_transit_gateway_route_table.spoke.id
+          aws_ec2_transit_gateway_route_table.ntc_hub.id,
+          aws_ec2_transit_gateway_route_table.ntc_spoke.id
         ]
       }
       # (optional) share subnet with Organizations, OUs or Accounts - requires RAM to be enabled for Organizations
