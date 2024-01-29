f64d88042a687fa0f08907482d343dbc6a0d0200
stefano-franco <stefano.franco@nuvibit.com>
2024-01-29 17:12:06 +0100

    add core network module

--- ntc_core_network.tf
+++ ntc_core_network.tf
@@ -36,9 +36,32 @@ resource "aws_ec2_transit_gateway_route_table" "ntc_spoke" {
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ NTC CORE NETWORK
 # ---------------------------------------------------------------------------------------------------------------------
-# module "ntc_core_network" {
-#   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-core-network?ref=beta"
-#   providers = {
-#     aws = aws.euc1
-#   }
-# }
+module "ntc_core_network" {
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-core-network?ref=beta"
+
+  transit_gateway = {
+    name                            = "tgw-core-frankfurt"
+    description                     = "core network in frankfurt"
+    amazon_side_asn                 = 64512
+    default_route_table_association = true
+    default_route_table_propagation = true
+    dns_support                     = true
+    multicast_support               = false
+    vpn_ecmp_support                = true
+    cidr_blocks                     = []
+    route_table_names = [
+      "tgw-route-table-hub",
+      "tgw-route-table-spoke-prod",
+      "tgw-route-table-spoke-dev",
+      "tgw-route-table-spoke-int",
+    ]
+    # (optional) share subnet with Organizations, OUs or Accounts - requires RAM to be enabled for Organizations
+    auto_accept_shared_attachments      = false
+    ram_share_principals                = []
+    ram_share_allow_external_principals = false
+  }
+
+  providers = {
+    aws = aws.euc1
+  }
+}
