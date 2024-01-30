e1f581d4b241dc5eaae1f3983280a512ac49f6bd
stefano-franco <stefano.franco@nuvibit.com>
2024-01-30 16:46:16 +0100

    add placeholder

--- ntc_core_network.tf
+++ ntc_core_network.tf
@@ -65,3 +65,61 @@ module "ntc_core_network" {
     aws = aws.euc1
   }
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ¦ NTC CORE NETWORK - CUSTOM ROUTES
+# ---------------------------------------------------------------------------------------------------------------------
+# module "ntc_core_network_custom_routes" {
+#   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-core-network//modules/custom-routes?ref=beta"
+
+#   # add custom routes for more flexibility and full control (e.g. firewall deployment)
+#   custom_routes = [
+#     {
+#       # route table where custom route will be be added
+#       route_table_id = module.ntc_vpc_prod_stage.route_table_ids["cloudonly-private"][0]
+#       # what is the destination of the traffic that should be controlled by this route?
+#       # a single destination type is required and cannot combine multiple destination types
+#       destination = {
+#         cidr_block      = "10.100.10.0/24"
+#         ipv6_cidr_block = ""
+#         prefix_list_id  = ""
+#       }
+#       # what is the target of the traffic that should be controlled by this route?
+#       # a single target type is required and cannot combine multiple target types
+#       target = {
+#         carrier_gateway_id          = ""
+#         core_network_arn            = ""
+#         ipv6_egress_only_gateway_id = ""
+#         internet_gateway_id         = ""
+#         transit_gateway_id          = ""
+#         virtual_private_gateway_id  = ""
+#         vpc_peering_connection_id   = ""
+#         nat_gateway_id              = ""
+#         network_interface_id        = "eni-068b5ccd7f7b7cfd3"
+#         vpc_endpoint_id             = ""
+#       }
+#     },
+#     {
+#       route_table_id = module.ntc_vpc_prod_stage.route_table_ids["cloudonly-private"][1]
+#       destination = {
+#         cidr_block = "10.100.10.0/24"
+#       }
+#       target = {
+#         network_interface_id = "eni-0ca9af96faf51d443"
+#       }
+#     },
+#     {
+#       route_table_id = module.ntc_vpc_prod_stage.route_table_ids["cloudonly-private"][2]
+#       destination = {
+#         cidr_block = "10.100.10.0/24"
+#       }
+#       target = {
+#         network_interface_id = "eni-0e55b3e0b04ee1824"
+#       }
+#     }
+#   ]
+
+#   providers = {
+#     aws = aws.euc1
+#   }
+# }
