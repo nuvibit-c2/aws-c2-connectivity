9416d63ac5ef223480b9b22a22d689aa9742fb58
stefano-franco <stefano.franco@nuvibit.com>
2024-02-13 14:34:41 +0100

    update ipam module

--- ntc_ipam.tf
+++ ntc_ipam.tf
@@ -2,7 +2,7 @@
 # ¦ NTC IPAM
 # ---------------------------------------------------------------------------------------------------------------------
 module "ipam" {
-  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-ipam?ref=beta"
+  source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-ipam?ref=1.0.0"
 
   description = "Organizational IPAM"
 
