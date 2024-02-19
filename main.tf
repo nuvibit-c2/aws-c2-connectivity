b351d7434378fb78f6df01d9fac1d3ff201db291
stefano-franco <stefano.franco@nuvibit.com>
2024-02-19 20:40:25 +0100

    remove unused data source

--- main.tf
+++ main.tf
@@ -52,9 +52,9 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 data "aws_region" "default" {}
 data "aws_caller_identity" "current" {}
-data "aws_availability_zones" "available" {
-  state = "available"
-}
+# data "aws_availability_zones" "available" {
+#   state = "available"
+# }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ¦ LOCALS
