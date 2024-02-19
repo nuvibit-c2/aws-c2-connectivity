abafba7214129440055a81d5cc673787e310b943
stefano-franco <stefano.franco@nuvibit.com>
2024-02-19 20:57:01 +0100

    remove placeholder

--- ntc_core_network.tf
+++ ntc_core_network.tf
@@ -1,39 +1,4 @@
 # ---------------------------------------------------------------------------------------------------------------------
-# ¦ PLACEHOLDER: NTC-CORE-NETWORK
-# ---------------------------------------------------------------------------------------------------------------------
-resource "aws_ec2_transit_gateway" "ntc_core" {
-  description                     = "core network"
-  amazon_side_asn                 = 64512
-  auto_accept_shared_attachments  = "disable"
-  default_route_table_association = "enable"
-  default_route_table_propagation = "enable"
-  dns_support                     = "enable"
-  multicast_support               = "disable"
-  transit_gateway_cidr_blocks     = []
-  vpn_ecmp_support                = "enable"
-
-  tags = {
-    "Name" = "tgw-core"
-  }
-}
-
-resource "aws_ec2_transit_gateway_route_table" "ntc_hub" {
-  transit_gateway_id = aws_ec2_transit_gateway.ntc_core.id
-
-  tags = {
-    "Name" = "tgw-route-table-hub"
-  }
-}
-
-resource "aws_ec2_transit_gateway_route_table" "ntc_spoke" {
-  transit_gateway_id = aws_ec2_transit_gateway.ntc_core.id
-
-  tags = {
-    "Name" = "tgw-route-table-spoke"
-  }
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
 # ¦ NTC CORE NETWORK
 # ---------------------------------------------------------------------------------------------------------------------
 module "ntc_core_network" {
