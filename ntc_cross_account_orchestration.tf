4623cd2a4690f5fc0e547fafeb0bcc0dba4573e9
stefano-franco <stefano.franco@nuvibit.com>
2024-05-27 16:59:02 +0200

    adding orchestration rules

--- ntc_cross_account_orchestration.tf
+++ ntc_cross_account_orchestration.tf
@@ -4,11 +4,11 @@
 module "ntc_cross_account_orchestration" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-cross-account-orchestration?ref=beta"
 
-  # this bucket stores information for cross account orchestration which will be provided by member accounts
-  orchestration_bucket_name = "ntc-cross-account-orchestration-connectivity"
-
   # organization id to limit bucket access to organization accounts
   org_id = local.ntc_parameters["mgmt-organizations"]["org_id"]
+
+  # this bucket stores information for cross account orchestration which will be provided by member accounts
+  orchestration_bucket_name = "ntc-cross-account-orchestration-connectivity"
 
   # notify on orchestration step function errors
   orchestration_notification_settings = {
@@ -23,24 +23,43 @@ module "ntc_cross_account_orchestration" {
     ]
   }
 
-  # orchestration_step_function_settings = {
-  #   # this function will create subdomain delegation records in a central hosted zone
-  #   route53_subdomain_delegation = {
-  #     enabled        = true
-  #     s3_file_prefix = "r53_delegation"
-  #     root_zone_id   = ""
-  #     root_zone_name = ""
-  #   }
-  #   # this function will add route table asssociations and propagations to vpc attachment in a central transit gateway
-  #   transit_gateway_vpc_attachment = {
-  #     enabled                           = true
-  #     s3_file_prefix                    = "tgw_attachment"
-  #     transit_gateway_id                = ""
-  #     transit_gateway_association_rules = []
-  #     transit_gateway_propagation_rules = []
-  #   }
-  #   # TODO: should orchestration also get organizational data like security tooling? caching dynamodb would be required
-  # }
+  # trigger orchestration when a organization member account creates a JSON file in the orchestration_bucket and a rule matches
+  orchestration_rules = [
+    {
+      rule_name      = "r53_subdomain_delegation_workloads"
+      rule_type      = "route53_subdomain_delegation"
+      s3_file_prefix = "r53_delegation"
+      # orchestrate cross-account route53 public subdomain delegation
+      route53_delegation_settings = {
+        root_zone_id   = module.ntc_route53_nuvibit_dev.zone_id
+        dnssec_enabled = true
+      }
+      # by default this rule will apply to all accounts where 's3_file_prefix' matches
+      condition = {}
+    },
+    {
+      rule_name      = "tgw_attachment_workloads_prod"
+      rule_type      = "transit_gateway_vpc_attachment"
+      s3_file_prefix = "tgw_attachment"
+      # orchestrate cross-account transit gateway vpc attachments associations and propagations
+      transit_gateway_vpc_attachment_settings = {
+        transit_gateway_id            = module.ntc_core_network_frankfurt.transit_gateway_id
+        associate_with_route_table_id = module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-prod"]
+        propagate_to_route_table_ids = [
+          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-hub"],
+          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-prod"],
+          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-onprem"]
+        ]
+      }
+      # condition to limit to which accounts this rule applies where 's3_file_prefix' also matches
+      condition = {
+        test     = "StringEquals" # StringEquals, StringLike
+        variable = "ou_path"      # account_id, account_name, ou_path, account_tag/KEY_NAME
+        values   = ["/root/workloads/prod"]
+      }
+    }
+  ]
+  # TODO: should orchestration also get organizational read access like security tooling? caching dynamodb would be required
 
   providers = {
     aws = aws.euc1
