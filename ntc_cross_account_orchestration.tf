d051e42e981dd197d71492e4c6bb27f35b2ebf69
stefano-franco <stefano.franco@nuvibit.com>
2024-06-06 16:48:59 +0200

    remove orchestration rules

--- ntc_cross_account_orchestration.tf
+++ ntc_cross_account_orchestration.tf
@@ -28,68 +28,68 @@ module "ntc_cross_account_orchestration" {
 
   # trigger orchestration when a organization member account creates a JSON file in the orchestration_bucket and a rule matches
   orchestration_rules = [
-    {
-      rule_name          = "r53_subdomain_delegation_workloads"
-      orchestration_type = "route53_subdomain_delegation"
-      s3_file_prefix     = "r53_delegation/"
-      # orchestrate cross-account route53 public subdomain delegation
-      route53_delegation_settings = {
-        root_zone_id             = module.ntc_route53_nuvibit_dev.zone_id
-        ns_record_ttl_in_seconds = 3600 # 1h = 3600; 1d = 86400
-        ds_record_ttl_in_seconds = 3600 # 1h = 3600; 1d = 86400
-        dnssec_enabled           = true
-        # (optional) subdomain zone name must match value specified in account tag
-        subdomain_equals_account_tag = "AccountDNSZoneName"
-        # (optional) separator when multiple zones are specified in account tag
-        subdomain_separator_account_tag = " " # e.g. "app1.example.com app2.example.com"
-      }
-      # by default orchestration_rules will apply to all accounts where 's3_file_prefix' matches
-      condition = {}
-    },
-    {
-      rule_name          = "tgw_attachment_workloads_prod"
-      orchestration_type = "transit_gateway_vpc_attachment"
-      s3_file_prefix     = "tgw_attachment/"
-      # orchestrate cross-account transit gateway vpc attachments associations and propagations
-      transit_gateway_vpc_attachment_settings = {
-        transit_gateway_id            = module.ntc_core_network_frankfurt.transit_gateway_id
-        associate_with_route_table_id = module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-prod"]
-        propagate_to_route_table_ids = [
-          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-hub"],
-          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-prod"],
-          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-onprem"]
-        ]
-      }
-      # by default orchestration_rules will apply to all accounts where 's3_file_prefix' matches
-      # a condition can additionaly restrict to which accounts orchestration_rules will apply
-      condition = {
-        test     = "StringEquals" # StringEquals, StringLike
-        variable = "ouPath"       # accountId, accountName, ouPath, accountTag:KEY_NAME
-        values   = ["/root/workloads/prod"]
-      }
-    },
-    {
-      rule_name          = "tgw_attachment_workloads_dev"
-      orchestration_type = "transit_gateway_vpc_attachment"
-      s3_file_prefix     = "tgw_attachment/"
-      # orchestrate cross-account transit gateway vpc attachments associations and propagations
-      transit_gateway_vpc_attachment_settings = {
-        transit_gateway_id            = module.ntc_core_network_frankfurt.transit_gateway_id
-        associate_with_route_table_id = module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-dev"]
-        propagate_to_route_table_ids = [
-          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-hub"],
-          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-dev"],
-          module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-onprem"]
-        ]
-      }
-      # by default orchestration_rules will apply to all accounts where 's3_file_prefix' matches
-      # a condition can additionaly restrict to which accounts orchestration_rules will apply
-      condition = {
-        test     = "StringEquals" # StringEquals, StringLike
-        variable = "ouPath"       # accountId, accountName, ouPath, accountTag:KEY_NAME
-        values   = ["/root/workloads/dev"]
-      }
-    }
+    # {
+    #   rule_name          = "r53_subdomain_delegation_workloads"
+    #   orchestration_type = "route53_subdomain_delegation"
+    #   s3_file_prefix     = "r53_delegation/"
+    #   # orchestrate cross-account route53 public subdomain delegation
+    #   route53_delegation_settings = {
+    #     root_zone_id             = module.ntc_route53_nuvibit_dev.zone_id
+    #     ns_record_ttl_in_seconds = 3600 # 1h = 3600; 1d = 86400
+    #     ds_record_ttl_in_seconds = 3600 # 1h = 3600; 1d = 86400
+    #     dnssec_enabled           = true
+    #     # (optional) subdomain zone name must match value specified in account tag
+    #     subdomain_equals_account_tag = "AccountDNSZoneName"
+    #     # (optional) separator when multiple zones are specified in account tag
+    #     subdomain_separator_account_tag = " " # e.g. "app1.example.com app2.example.com"
+    #   }
+    #   # by default orchestration_rules will apply to all accounts where 's3_file_prefix' matches
+    #   condition = {}
+    # },
+    # {
+    #   rule_name          = "tgw_attachment_workloads_prod"
+    #   orchestration_type = "transit_gateway_vpc_attachment"
+    #   s3_file_prefix     = "tgw_attachment/"
+    #   # orchestrate cross-account transit gateway vpc attachments associations and propagations
+    #   transit_gateway_vpc_attachment_settings = {
+    #     transit_gateway_id            = module.ntc_core_network_frankfurt.transit_gateway_id
+    #     associate_with_route_table_id = module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-prod"]
+    #     propagate_to_route_table_ids = [
+    #       module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-hub"],
+    #       module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-prod"],
+    #       module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-onprem"]
+    #     ]
+    #   }
+    #   # by default orchestration_rules will apply to all accounts where 's3_file_prefix' matches
+    #   # a condition can additionaly restrict to which accounts orchestration_rules will apply
+    #   condition = {
+    #     test     = "StringEquals" # StringEquals, StringLike
+    #     variable = "ouPath"       # accountId, accountName, ouPath, accountTag:KEY_NAME
+    #     values   = ["/root/workloads/prod"]
+    #   }
+    # },
+    # {
+    #   rule_name          = "tgw_attachment_workloads_dev"
+    #   orchestration_type = "transit_gateway_vpc_attachment"
+    #   s3_file_prefix     = "tgw_attachment/"
+    #   # orchestrate cross-account transit gateway vpc attachments associations and propagations
+    #   transit_gateway_vpc_attachment_settings = {
+    #     transit_gateway_id            = module.ntc_core_network_frankfurt.transit_gateway_id
+    #     associate_with_route_table_id = module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-dev"]
+    #     propagate_to_route_table_ids = [
+    #       module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-hub"],
+    #       module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-spoke-dev"],
+    #       module.ntc_core_network_frankfurt.transit_gateway_route_table_ids["tgw-core-rtb-onprem"]
+    #     ]
+    #   }
+    #   # by default orchestration_rules will apply to all accounts where 's3_file_prefix' matches
+    #   # a condition can additionaly restrict to which accounts orchestration_rules will apply
+    #   condition = {
+    #     test     = "StringEquals" # StringEquals, StringLike
+    #     variable = "ouPath"       # accountId, accountName, ouPath, accountTag:KEY_NAME
+    #     values   = ["/root/workloads/dev"]
+    #   }
+    # }
   ]
 
   providers = {
