bfb2cfb89e2428e2c59cb6fa522ea1b425a89ab0
burib <burib@users.noreply.github.com>
2025-10-08 17:36:34 +0200

    feat: create c1.nuvibit.dev subzone delegation (#39)
    * feat: create c1.nuvibit.dev subzone delegation
    
    * style(terraform fmt): format code
    
    * docs(terraform-docs): update tfdocs
    
    ---------
    
    Co-authored-by: github-actions <noreply@github.com>

--- ntc_route53_public_hosted_zone.tf
+++ ntc_route53_public_hosted_zone.tf
@@ -51,8 +51,20 @@ module "ntc_route53_nuvibit_dev" {
         "ns-1608.awsdns-09.co.uk.",
         "ns-1027.awsdns-00.org.",
       ]
-      dnssec_enabled = true
+      dnssec_enabled   = true
       dnssec_ds_record = "13790 13 2 4ADEA6D37A6064708DCAE2BF1298A30A54EEAE4B43A22354446236353C916343"
+    },
+    {
+      # c1 zone delegation at 'aws-c1' ( https://github.com/nuvibit-c1/aws-c1-connectivity/blob/main/ntc_route53.tf )
+      subdomain_zone_name = "c1"
+      subdomain_nameserver_list = [
+        "ns-1134.awsdns-13.org",
+        "ns-1873.awsdns-42.co.uk",
+        "ns-712.awsdns-25.net",
+        "ns-72.awsdns-09.com",
+      ]
+      dnssec_enabled   = true
+      dnssec_ds_record = "48427 13 2 01A4F6DF790335CDBFCBB6C6E8160E45BF50A707B5D061D2F66928EC1CCB5FEC"
     },
   ]
 
