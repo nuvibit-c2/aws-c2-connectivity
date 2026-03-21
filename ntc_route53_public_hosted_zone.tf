7c483ea66da1e2373af6701d03b85241cfbff92d
stefano-franco <stefano.franco@nuvibit.com>
2026-03-21 22:42:10 +0100

    feat(ntc_route53): update zone delegation configuration and clean up commented code

--- ntc_route53_public_hosted_zone.tf
+++ ntc_route53_public_hosted_zone.tf
@@ -31,19 +31,7 @@ module "ntc_route53_nuvibit_dev" {
   # https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/dns-routing-traffic-for-subdomains.html
   zone_delegation_list = [
     {
-      # c1 zone delegation 'aws-c1-connectivity'
-      subdomain_zone_name = "c1"
-      subdomain_nameserver_list = [
-        "ns-1134.awsdns-13.org",
-        "ns-1873.awsdns-42.co.uk",
-        "ns-712.awsdns-25.net",
-        "ns-72.awsdns-09.com",
-      ]
-      dnssec_enabled   = true
-      dnssec_ds_record = "48427 13 2 01A4F6DF790335CDBFCBB6C6E8160E45BF50A707B5D061D2F66928EC1CCB5FEC"
-    },
-    {
-      # Stategraph PoC 'aws-c1-platform-tooling'
+      # 'stategraph.nuvibit.dev' zone delegation 'aws-c1-platform-tooling'
       subdomain_zone_name = "stategraph"
       subdomain_nameserver_list = [
         "ns-198.awsdns-24.com.",
@@ -55,17 +43,25 @@ module "ntc_route53_nuvibit_dev" {
       dnssec_ds_record = "24965 13 2 05B8F914A93AEC3B95B684FF52E4240BCDCE17200B542824E54C4681CE9001D9"
     },
     {
-      # NTC Summit Demo 'aws-c3'
-      subdomain_zone_name = "summit"
+      # 'c1.nuvibit.dev' zone delegation 'aws-c1-connectivity'
+      subdomain_zone_name = "c1"
       subdomain_nameserver_list = [
-        "ns-460.awsdns-57.com.",
-        "ns-676.awsdns-20.net.",
-        "ns-1608.awsdns-09.co.uk.",
-        "ns-1027.awsdns-00.org.",
+        "ns-1134.awsdns-13.org",
+        "ns-1873.awsdns-42.co.uk",
+        "ns-712.awsdns-25.net",
+        "ns-72.awsdns-09.com",
       ]
       dnssec_enabled   = true
-      dnssec_ds_record = "13790 13 2 4ADEA6D37A6064708DCAE2BF1298A30A54EEAE4B43A22354446236353C916343"
+      dnssec_ds_record = "48427 13 2 01A4F6DF790335CDBFCBB6C6E8160E45BF50A707B5D061D2F66928EC1CCB5FEC"
     },
+    # {
+    #   # 'c3.nuvibit.dev' zone delegation 'aws-c3-connectivity'
+    #   subdomain_zone_name = "c3"
+    #   subdomain_nameserver_list = [
+    #   ]
+    #   dnssec_enabled   = true
+    #   dnssec_ds_record = ""
+    # },
   ]
 
   dnssec_config = {
