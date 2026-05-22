5f134dff192932023684917128566fb098cd2391
stefano-franco <stefano.franco@nuvibit.com>
2026-05-22 22:38:17 +0200

    chore: update aws product tag

--- main.tf
+++ main.tf
@@ -39,5 +39,7 @@ locals {
   default_tags = {
     ManagedBy     = "OpenTofu"
     ProvisionedBy = "aws-c2-connectivity"
+    # aws product tag
+    aws-apn-id = "pc:c1ztn0gbrzi3judwmfx1lrq1t"
   }
 }
